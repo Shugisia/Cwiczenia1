ee345085b0e13f4734d4afdffff7336dd91c6078
Wanessa Witkowska <128226892+Shugisia@users.noreply.github.com>
2023-03-19 11:35:31 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//using System;
+//namespace Projekt1_Witkowska 
+//{
+//  internal class Program 
+//  {
+//      static void Main(string[] args)
+//      {
+//          Console.WriteLine("Hello, World!");
+//      }
+//   }
+//}
+
+Console.WriteLine("Hello, World!");
